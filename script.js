5debb8e5e6d7a44c0961d04966878b80f2f80b45
João Victor Kulka <jv.kulka13@gmail.com>
2022-12-26 18:20:41 -0300

    Finalizado sidebar responsiva | Realizado demais ajustes.

--- script.js
+++ script.js
@@ -47,32 +47,38 @@ navegacaoAnimacaoScroll()
 
 // Criação de animação do header responsivo
 
-let header = document.getElementById('header')
-let navegacao = document.getElementById('navegacao')
-let sessaoInicial = document.getElementById('sessao-inicial')
-let showSidebar = false
+let header2 = document.getElementById('header');
+let navigationHeader = document.getElementById('navigation_header');
+let sessaoInicial = document.getElementById('sessao-inicial');
+let sessaoSobreMim = document.getElementById('sessao-sobre-mim');
+let showSidebar2 = false;
 
-function abrirBotao() {
-    showSidebar = !showSidebar
-    if (showSidebar) {
-        navegacao.style.marginLeft = '-10vw'
-        navegacao.style.animationName = 'showSidebar'
-        sessaoInicial.style.filter = 'blur(2px)'
-    } else {
-        navegacao.style.marginLeft = '-100vw'
-        navegacao.style.animationName = ''
-        sessaoInicial.style.filter = ''
+function toggleSidebar() {
+    showSidebar2 = !showSidebar2;
+    if (showSidebar2) {
+        navigationHeader.style.marginLeft = '-10vw';
+        navigationHeader.style.animationName = 'showSidebar';
+        sessaoInicial.style.filter = 'blur(2px)';
+        sessaoSobreMim.style.filter = 'blur(2px)';
+    }
+    else {
+        navigationHeader.style.marginLeft = '-100vw';
+        navigationHeader.style.animationName = '';
+        sessaoInicial.style.filter = '';
+        sessaoSobreMim.style.filter = '';
     }
 }
 
 function closeSidebar() {
-    if (showSidebar) {
-        abrirBotao()
+    if (showSidebar2) {
+        showSidebar2 = true;
+        toggleSidebar();
     }
 }
 
 window.addEventListener('resize', function (event) {
-    if (this.window.innerWidth > 768 && showSidebar) {
-        abrirBotao()
+    if (window.innerWidth > 768 && showSidebar2) {
+        showSidebar2 = true;
+        toggleSidebar();
     }
-})
+});
